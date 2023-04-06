66ea1db2d5caaedb683d4c4c73003c8e806d8308
Muhammad Jaffar <129592007+uploadgithub@users.noreply.github.com>
2023-04-06 16:10:23 +0500

    Add files via upload

---
+++ university website/uni.js
@@ -0,0 +1,27 @@
+
+/*var navLinks=document.getElementById("navLinks");
+function showmenu(){
+navLinks.style.right="0";
+}
+function hidemenu(){
+navLinks.style.right="-200px";
+}
+
+
+//DOM GET AND SET    THE ATTRIBUTE
+
+// INNER TEXT     AND HTML
+
+var element;
+
+element = document.getElementsByClassName("course").innerHTMLt;
+
+console.log(element);  */
+
+//GET ATTRIBUTE
+
+var element;
+
+element = document.getElementsByClassName("links").getAttribute;
+
+console.log(element);
